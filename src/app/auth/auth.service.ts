920d121d066c3671c65768cd28f1d7e94cc43236
Cannon-Cloud <clarence@cannon.cloud>
2021-07-19 13:11:36 -0700

    Basic Authentication Complete

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,7 +5,13 @@ import { AuthData } from './auth-data.modal';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+  private token: string;
+
   constructor(private http: HttpClient) {}
+
+  getToken() {
+    return this.token;
+  }
 
   createUser(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
@@ -13,6 +19,19 @@ export class AuthService {
       .post('http://localhost:3000/api/users/signup', authData)
       .subscribe((response) => {
         console.log(response);
+      });
+  }
+
+  login(email: string, password: string) {
+    const authData: AuthData = { email: email, password: password };
+    this.http
+      .post<{ token: string }>(
+        'http://localhost:3000/api/users/login',
+        authData
+      )
+      .subscribe((response) => {
+        const token = response.token;
+        this.token = token;
       });
   }
 }

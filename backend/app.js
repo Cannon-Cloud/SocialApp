45e1ffcad16edd12aecca52c5c465201e9603d03
Cannon-Cloud <clarence@cannon.cloud>
2021-07-20 11:07:34 -0700

    added backend global values

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,9 @@ const app = express();
 
 mongoose
   .connect(
-    "mongodb+srv://ccdbuser:P0DFyFvqH5aTUO0P@cluster0.mgrua.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    "mongodb+srv://ccdbuser:" +
+      process.env.MONGO_ATLAS_PW +
+      "@cluster0.mgrua.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
   )
   .then(() => {
     console.log("Connected to database!");
